a8708a4031367a1dbbefd41aeb1f7370a9afe199
sanjit poudel <sapo@pc-tec.local>
2022-09-14 09:25:47 +0200

    Commiting

--- PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PasswordGenerator
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameText.Text}");
+        }
     }
 }
